68003bf76f637ac825bd32c904edf405345a59a9
Akash Gangurde <ganaka01@csgicorp.com>
2023-02-27 17:54:32 +0530

    new scripts added

---
+++ scripts/V1.1.1__t_ride_data.sql
@@ -0,0 +1,14 @@
+create or replace table T_RIDE_DATA
+(ride_id VARCHAR,
+ rideable_type VARCHAR,
+ started_at timestamp,
+ ended_at timestamp,
+ start_station_name VARCHAR,
+ start_station_id VARCHAR,
+ end_station_name VARCHAR,
+ end_station_id VARCHAR,
+ start_lat VARCHAR,
+ start_lng VARCHAR,
+ end_lat VARCHAR,
+ end_lng VARCHAR,
+ membership_type VARCHAR);
